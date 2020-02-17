01c9f167b04a208c474636350f73463e16aecf0b
Yuan Su <bossyuansu@gmail.com>
2020-02-17 13:24:25 -0500

    add infura support

--- migrations/2_deploy_mis_token.js
+++ migrations/2_deploy_mis_token.js
@@ -3,13 +3,19 @@ const MSC = artifacts.require("MSC");
 require('@openzeppelin/test-helpers/configure')({ provider: web3.currentProvider, environment: 'truffle' });
 
 const { singletons } = require('@openzeppelin/test-helpers');
+const BN = require('bignumber.js');
 
 module.exports = async function(deployer, network, accounts) {
   if (network === 'development') {
       // In a test environment an ERC777 token requires deploying an ERC1820 registry
       await singletons.ERC1820Registry(accounts[0]);
   }
-  await deployer.deploy(MIS, '1000000000000000', [],[],[]);
-  const token = await MIS.deployed();
-  await deployer.deploy(MSC, [accounts[1], accounts[2]], accounts[0], 1, token.address, 10000);
+  const tokenbits = (new BN(10)).pow(18); 
+  const amount = (new BN(10000)).multipliedBy(tokenbits); 
+
+  await deployer.deploy(MIS, amount, [],[],[]);
+  if (network === 'development') {
+      const token = await MIS.deployed();
+      await deployer.deploy(MSC, [accounts[1], accounts[2]], accounts[0], 1, token.address, 10000);
+  }
 };
--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "f484a9042ea748239afd432c8cba5a40";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -60,14 +60,14 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    ropsten: {
+            provider: () => new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/f484a9042ea748239afd432c8cba5a40"),
+       network_id: 3,       // Ropsten's id
+       gas: 5500000,        // Ropsten has a lower block limit than mainnet
+       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+       skipDryRun: false    // Skip dry run before migrations? (default: false for public nets )
+     },
 
     // Useful for private networks
     // private: {
