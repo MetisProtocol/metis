c2e65a74ffd6ee930f01a2e15f04644456966e22
Yuan Su <bossyuansu@gmail.com>
2020-02-16 19:24:35 -0500

    bug fixes, add IMSC

---
+++ contracts/IMSC.sol
@@ -0,0 +1,75 @@
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
+import "@openzeppelin/contracts/token/ERC777/IERC777Sender.sol";
+import "@openzeppelin/contracts/access/Roles.sol";
+
+/**
+ * @dev interface of MSC
+ */
+interface IMSC {
+
+    event Transaction (address operator, address from, address to, uint256 amount, bytes msg1, bytes msg2);
+    event ContractClose(address initiator, uint lastStatusChange, uint numWantedout, bytes msg);
+    event ContractDispute(address operator, uint lastStatusChange, bytes msg);
+    event DisputeResolution(address facilitator, uint lastStatusChange, address[] participants, uint[] values);
+    
+    /**
+     * @dev commit funds to the contract. participants can keep committing after the pledge ammount is reached
+     * @param amount amount of fund to commit
+     * The sender must authorized this contract to be the operator of senders account before committing
+     */
+    function commit(uint256 amount) external; 
+
+    /**
+     * @dev transfer pledge value from one participant to another
+     * @param to target address
+     * @param amount amount of fund to transfer
+     * The sender must have enough fund pledged. The method does not trigger status changes.
+     * The transaction is not allowed if the contract is in the middle of a dispute.
+     */
+    function send(address to, uint256 amount) external;
+
+    /**
+     * @dev withdraw the pledged fund
+     * The sender must have enough fund pledged. Contract will be closed if all funds are withdrawn
+     */
+    function withdraw() external;
+   
+    /**
+     * @dev signal a participant wants to exit
+     * @param check a flag to indicate whether the call is just to check the exit status
+     * once an exit is signaled, the contract will wait pledgePeriod * 1 days for other participants to react
+     * if other participants also signal exits, the contract will change the status to complete and allow withdraws
+     * the other participants can choose to dispute, which blocks the withdraws until the dispute is resolved
+     * if no dispute is resolved after the pledge period expires, the contract will automatically set to complete
+     * and open to withdraws.
+     * if check is true, the call may still cause status update if the pledge period expires
+     */
+    function iwantout(bool check) external;
+
+    /** 
+     * @dev raise a dispute
+     * only one participant can raise the dispute. it will put the contract status to dispute, blocking all
+     * further transactions.
+     */
+    function dispute() external;
+
+    /** 
+     * @dev cancel a dispute
+     * the method also reset the status change. the original dispute period continues.
+     */
+    function withdrawDispute() external;
+
+    /** 
+     * @dev resolve a dispute
+     * @param participants the list of addresses of participants involved in the resolution
+     * @param values the list of resolved values after the resolution
+     * Only the facilitator can resolve a dispute.
+     * The total amount of values cannot exceed the total funds pledged. The contract status will set to Completed,
+     * allowing withdraws.
+     */
+    function resolveDispute(uint[] calldata participants, uint[] calldata values) external;
+
+}
--- contracts/MSC.sol
+++ contracts/MSC.sol
@@ -4,48 +4,62 @@ import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
 import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
 import "@openzeppelin/contracts/token/ERC777/IERC777Sender.sol";
 import "@openzeppelin/contracts/access/Roles.sol";
+import "./IMSC.sol";
 
-contract MSC is ERC777Recipient{
+/**
+ * @dev implementation of MSC
+ */
+contract MSC is IERC777Recipient, IERC777Sender{
     using Roles for Roles.Role;
     enum ContractStatus { Pending, Effective, Completed, Dispute, Closed }
     enum ParticipantStatus { Pending,Committed, Wantout, Completed, Dispute, Closed }
+
     event Transaction (address operator, address from, address to, uint256 amount, bytes msg1, bytes msg2);
     event ContractClose(address initiator, uint lastStatusChange, uint numWantedout, bytes msg);
     event ContractDispute(address operator, uint lastStatusChange, bytes msg);
     event DisputeResolution(address facilitator, uint lastStatusChange, address[] participants, uint[] values);
     
     struct Pledge {
-        uint256 value;
+        uint256 value; 
         ParticipantStatus status;
     }
 
     Roles.Role private _participants;
     Roles.Role private _facilitators;
-    address _tokenAddr private;
-    uint lastStatusChange private;
-    uint numCommitted private;
-    uint numWantedout private;
-    uint256 pledgeAmount public;
-    uint disputePeriod public;
-    address disputeInitiator public;
+    address private _tokenAddr; //token contract address
+    uint private lastStatusChange;
+    uint private disputeRollBackDays;
+    uint private numCommitted;
+    uint private numWantedout;
+    uint256 public pledgeAmount;
+    uint public disputePeriod;
+    address public disputeInitiator;
 
     ContractStatus public contractStatus;
 
     mapping(address => Pledge) parties;
-    address[] participantsArray public;
-    address[] facilitatorsArray public; 
+    address[] public participantsArray;
+    address[] public facilitatorsArray; 
 
+    /**
+     * @dev participants cannot be empty
+     * @param participants list of participants
+     * @param facilitators list of facilitators, who can resolve disputes
+     * @param period period in days of time one can raise disputes after a participant requests the exit
+     * @param tokenAddr token contract address
+     * @param amount amount of pledge requred
+     */
     constructor(
 	    address[] memory participants,
 	    address[] memory facilitators,
         uint period,
-        address memory tokenAddr,
+        address tokenAddr,
         uint256 amount
     )
     public
     {
         participantsArray = participants;
-        facilitatorArray = facilitatos;
+        facilitatorsArray = facilitators;
         for (uint256 i = 0; i < participants.length; ++i) {
 	        _participants.add(participants[i]);
         }
@@ -57,6 +71,11 @@ contract MSC is ERC777Recipient{
         disputePeriod = period;
     }
 
+    /**
+     * @dev commit funds to the contract. participants can keep committing after the pledge ammount is reached
+     * @param amount amount of fund to commit
+     * The sender must authorized this contract to be the operator of senders account before committing
+     */
     function commit(uint256 amount) public {
 
         IERC777 erc777 = IERC777(_tokenAddr);
@@ -64,15 +83,14 @@ contract MSC is ERC777Recipient{
         // Only participants are allowed
         require(amount > 0, "AMOUNT_NOT_GREATER_THAN_ZERO");
         require(_participants.has(msg.sender), "DOES_NOT_HAVE_PARTICIPANT_ROLE");
-        require(p.status == ParticipantStatus.Pending, "STATUS_NOT_IN_PENDING");
         require(erc777.balanceOf(msg.sender) >= amount, "INSUFFICIENT_BALANCE");
         require(erc777.isOperatorFor(address(this), msg.sender), "NOT_AUTHORIZED_AS_OPERATOR");
         
         p.value += amount;
-        if (p.value >= pledgeAmount) {
+        if (p.value >= pledgeAmount && p.status == ParticipantStatus.Pending) {
                 p.status = ParticipantStatus.Committed;
                 numCommitted++;
-                if (numCommited == participantsArray.length) {
+                if (numCommitted == participantsArray.length) {
                         contractStatus = ContractStatus.Effective;
                 }
         }
@@ -80,6 +98,34 @@ contract MSC is ERC777Recipient{
         erc777.operatorSend(msg.sender, address(this), amount, "", "Pledge");
     }
 
+    /**
+     * @dev transfer pledge value from one participant to another
+     * @param to target address
+     * @param amount amount of fund to transfer
+     * The sender must have enough fund pledged. The method does not trigger status changes.
+     * The transaction is not allowed if the contract is in the middle of a dispute.
+     */
+    function send(address to, uint256 amount) public {
+
+        Pledge memory p = parties[msg.sender];
+        Pledge memory targetP = parties[to];
+
+        // Only participants are allowed
+        require(amount > 0, "AMOUNT_NOT_GREATER_THAN_ZERO");
+        require(_participants.has(msg.sender), "DOES_NOT_HAVE_PARTICIPANT_ROLE");
+        require(_participants.has(to), "TARGET_DOES_NOT_HAVE_PARTICIPANT_ROLE");
+        require(contractStatus != ContractStatus.Dispute, "CONTRACT_IS_IN_DISPUTE");
+        require(p.value >= amount, "INSUFFICIENT_BALANCE");
+        
+        p.value -= amount;
+        targetP.value += amount;
+        emit Transaction(msg.sender, msg.sender, to, amount, "", "Transfer");
+    }
+
+    /**
+     * @dev withdraw the pledged fund
+     * The sender must have enough fund pledged. Contract will be closed if all funds are withdrawn
+     */
     function withdraw() public {
         IERC777 erc777 = IERC777(_tokenAddr);
         Pledge memory p = parties[msg.sender];
@@ -91,17 +137,26 @@ contract MSC is ERC777Recipient{
         require(erc777.balanceOf(address(this)) >= p.value, "INSUFFICIENT_BALANCE");
         
         p.status = ParticipantStatus.Closed;
-        if (erc777.balanceOf(address(this) == p.value)) {
-                ContractStatus = ContractStatus.Closed;
+        if (erc777.balanceOf(address(this)) == p.value) {
+                contractStatus = ContractStatus.Closed;
         }
+        erc777.send(msg.sender,p.value, "Withdraw");
+
         p.value = 0;
         lastStatusChange = now;
-        erc777.send(msg.sender,amount, "", "Withdraw");
     }
    
+    /**
+     * @dev signal a participant wants to exit
+     * @param check a flag to indicate whether the call is just to check the exit status
+     * once an exit is signaled, the contract will wait pledgePeriod * 1 days for other participants to react
+     * if other participants also signal exits, the contract will change the status to complete and allow withdraws
+     * the other participants can choose to dispute, which blocks the withdraws until the dispute is resolved
+     * if no dispute is resolved after the pledge period expires, the contract will automatically set to complete
+     * and open to withdraws.
+     * if check is true, the call may still cause status update if the pledge period expires
+     */
     function iwantout(bool check) public {
-
-        IERC777 erc777 = IERC777(_tokenAddr);
         Pledge memory p = parties[msg.sender];
         // Only participants are allowed
         require(_participants.has(msg.sender), "DOES_NOT_HAVE_PARTICIPANT_ROLE");
@@ -112,11 +167,11 @@ contract MSC is ERC777Recipient{
                 p.status = ParticipantStatus.Wantout;
                 numWantedout++;
         }
-        if (numWantedout == participantsArray.length)
-                ContractStatus = ContractStatus.Completed;
+        if (numWantedout == participantsArray.length) {
+                contractStatus = ContractStatus.Completed;
                 emit ContractClose(msg.sender, lastStatusChange, numWantedout, "All Aggreed");
-        } else if (now >= lastStatusChange + disputePeriod * 1 day && ContractStatus != ContractStatus.Dispute) {
-                ContractStatus = ContractStatus.Completed;
+        } else if (now >= lastStatusChange + disputePeriod * 1 days && contractStatus != ContractStatus.Dispute) {
+                contractStatus = ContractStatus.Completed;
                 emit ContractClose(msg.sender, lastStatusChange, numWantedout, "Dispute Expired");
         }
         if (check == false) {
@@ -124,6 +179,11 @@ contract MSC is ERC777Recipient{
         }
     }
 
+    /** 
+     * @dev raise a dispute
+     * only one participant can raise the dispute. it will put the contract status to dispute, blocking all
+     * further transactions.
+     */
     function dispute() public {
         Pledge memory p = parties[msg.sender];
         // Only participants are allowed
@@ -134,10 +194,17 @@ contract MSC is ERC777Recipient{
         disputeInitiator = msg.sender;
         p.status = ParticipantStatus.Dispute;
         contractStatus = ContractStatus.Dispute;
+
+        // store how many days spent since the exit was raised
+        disputeRollBackDays = now - lastStatusChange;
         emit ContractDispute(msg.sender, lastStatusChange, "Initiated");
         lastStatusChange = now;
     }
 
+    /** 
+     * @dev cancel a dispute
+     * the method also reset the status change. the original dispute period continues.
+     */
     function withdrawDispute() public {
         Pledge memory p = parties[msg.sender];
         // Only participants are allowed
@@ -146,15 +213,25 @@ contract MSC is ERC777Recipient{
         require(contractStatus == ContractStatus.Dispute, "STATUS_IS_NOT_DISPUTE");
         require(p.status == ParticipantStatus.Dispute, "STATUS_NOT_IN_DISPUTE");
         
-        p.status = ParticipantStatus.Commited;
+        p.status = ParticipantStatus.Committed;
         contractStatus = ContractStatus.Effective;
         emit ContractDispute(msg.sender, lastStatusChange, "Withdrawn");
-        lastStatusChange = now;
+
+        // restore the date so the dispute period continues after the withdraw
+        lastStatusChange = now - disputeRollBackDays;
     }
 
+    /** 
+     * @dev resolve a dispute
+     * @param participants the list of addresses of participants involved in the resolution
+     * @param values the list of resolved values after the resolution
+     * Only the facilitator can resolve a dispute.
+     * The total amount of values cannot exceed the total funds pledged. The contract status will set to Completed,
+     * allowing withdraws.
+     */
     function resolveDispute(address[] memory participants, uint[] memory values) public {
         // Only participants are allowed
-        require(_facilitator.has(msg.sender), "DOES_NOT_HAVE_FACILITATOR_ROLE");
+        require(_facilitators.has(msg.sender), "DOES_NOT_HAVE_FACILITATOR_ROLE");
         require(contractStatus == ContractStatus.Dispute, "STATUS_IS_NOT_DISPUTE");
         uint256 totalvalue = 0; 
         contractStatus = ContractStatus.Completed;
@@ -180,18 +257,18 @@ contract MSC is ERC777Recipient{
             uint256 amount,
             bytes calldata userData,
             bytes calldata operatorData
-    ) {
+    ) external {
        emit Transaction(operator, from, to, amount, userData, operatorData); 
     }
 
-    function tokensToSend(
+    function tokensToSend (
             address operator,
             address from,
             address to,
             uint256 amount,
             bytes calldata userData,
             bytes calldata operatorData
-    ) {
+    ) external {
        emit Transaction(operator, from, to, amount, userData, operatorData); 
     }
 }
