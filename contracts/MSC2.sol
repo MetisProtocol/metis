b764f2eac6d164f47a318d6ea5f5d7a9949e93db
Yuan Su <bossyuansu@gmail.com>
2021-03-27 21:11:31 -0400

    yes

--- contracts/MSC2.sol
+++ contracts/MSC2.sol
@@ -8,9 +8,6 @@ import "./IMSC.sol";
  * @dev implementation of MSC
  */
 contract MSC2 is IMSC {
-    IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
-    bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
-
     using Roles for Roles.Role;
     enum ContractStatus { Pending, Effective, Completed, Dispute, Requested, Closed }
     enum ParticipantStatus { Pending,Committed, Wantout, Completed, Dispute, Closed }
@@ -36,7 +33,7 @@ contract MSC2 is IMSC {
     uint256 public pledgeAmount;
     uint public disputePeriod;
     address public disputeInitiator;
-    address payable public facilitator;
+    address public facilitator;
 
     ContractStatus public contractStatus;
 
@@ -53,7 +50,7 @@ contract MSC2 is IMSC {
      */
     constructor(
 	    address[] memory participants,
-	    address payable facilitator_param,
+	    address facilitator_param,
         uint period,
         address tokenAddr,
         uint256 amount
